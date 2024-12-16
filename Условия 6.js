82d4cfa3a2b78b7b2d3d1a0be9885b37531eefce
mrWafffffleh <waffleh@mail.ru>
2024-12-16 16:05:02 +0300

    Обновы

--- Условия 6.js
+++ Условия 6.js
@@ -7,6 +7,8 @@ let gold = document.getElementById("gold");
 let blue = document.getElementById("blue");
 let orange = document.getElementById("orange");
 let posgold = gold.getBoundingClientRect()
+let posblue = blue.getBoundingClientRect()
+let posorange = orange.getBoundingClientRect()
 function add1(){
     count = count + 1;
     step.innerText = count;
@@ -48,6 +50,9 @@ function butgold(){
     nums.classList.remove('orange')
     gold.classList.add('border')
     choose = 1
+    gold.classList.add("border")
+    blue.classList.remove("border")
+    orange.classList.remove("border")
 }
 function butblue(){
     move.classList.add('blue')
@@ -57,6 +62,9 @@ function butblue(){
     nums.classList.remove('orange')
     nums.classList.remove('gold')
     choose = 2
+    gold.classList.remove("border")
+    blue.classList.add("border")
+    orange.classList.remove("border")
 }
 function butorange(){
     move.classList.add('orange')
@@ -65,30 +73,92 @@ function butorange(){
     nums.classList.add('orange')
     nums.classList.remove('gold')
     nums.classList.remove('blue')
-    choose = 2
+    choose = 3
+    gold.classList.remove("border")
+    blue.classList.remove("border")
+    orange.classList.add("border")
 }
 function button_left(){
     if(choose == 0){
         alert("Выберите блок");
     }
     else if(choose == 1){
-        gold.style.left = posgold.left + count + "px"
-        console.log(count);
+        let posgold = gold.getBoundingClientRect()
+        gold.style.left = posgold.left - count + "px"
+        if(posgold.left == 0){
+            gold.style.left = 0px
+
+        }
+    }
+    else if(choose == 2){
+        let posblue = blue.getBoundingClientRect()
+        blue.style.left = posblue.left - count + "px"
+
+    }
+    else if(choose == 3){
+        let posorange = orange.getBoundingClientRect()
+        orange.style.left = posorange.left - count + "px"
+
     }
 }
 function button_right(){
     if(choose == 0){
         alert("Выберите блок")
     }
+    else if(choose == 1){
+        let posgold = gold.getBoundingClientRect()
+        gold.style.left = posgold.left + count + "px"
+
+    }
+    else if(choose == 2){
+        let posblue = blue.getBoundingClientRect()
+        blue.style.left = posblue.left + count + "px"
+
+    }
+    else if(choose == 3){
+        let posorange = orange.getBoundingClientRect()
+        orange.style.left = posorange.left + count + "px"
+
+    }
 }
 function button_up(){
     if(choose == 0){
         alert("Выберите блок")
     }
+    else if(choose == 1){
+        let posgold = gold.getBoundingClientRect()
+        gold.style.top = posgold.top - count + "px"
+
+    }
+    else if(choose == 2){
+        let posblue = blue.getBoundingClientRect()
+        blue.style.top = posblue.top - count + "px"
+
+    }
+    else if(choose == 3){
+        let posorange = orange.getBoundingClientRect()
+        orange.style.top = posorange.top - count + "px"
+
+    }
 }
 function button_down(){
     if(choose == 0){
         alert("Выберите блок")
+    }
+    else if(choose == 1){
+        let posgold = gold.getBoundingClientRect()
+        gold.style.top = posgold.top + count + "px"
+
+    }
+    else if(choose == 2){
+        let posblue = blue.getBoundingClientRect()
+        blue.style.top = posblue.top + count + "px"
+
+    }
+    else if(choose == 3){
+        let posorange = orange.getBoundingClientRect()
+        orange.style.top = posorange.top + count + "px"
+
     }
 }
 function clearall(){
@@ -100,4 +170,5 @@ function clearall(){
     nums.classList.remove('orange')
     choose = 0
     step.innerText = " "
+    count = 0
 }

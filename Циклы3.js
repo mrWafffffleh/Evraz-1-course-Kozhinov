7cdbb2a948ade259dfbcc2992a584c4cccac313f
mrWafffffleh <waffleh@mail.ru>
2025-02-03 18:00:34 +0500

    Обновы

--- Циклы3.js
+++ Циклы3.js
@@ -1,10 +1,22 @@
-let a = []
 let i = 0
 while(i!=4) {
     i = prompt("1 - красный блок, 2 - синий блок, 3 - зеленый блок, 4 - стоп игра")
-    a.push(i)
-}
-len = a.length
-for(let b = 1; b <= len; b++) {
-    addElement = document.getElementById('all');
+    if(i == 1){
+        let AddElement = document.getElementById("all")
+        let divCreate = document.createElement("div")
+        divCreate.classList.add("red");
+        AddElement.append(divCreate);
+    }
+    else if(i == 2){
+        let AddElement = document.getElementById("all")
+        let divCreate = document.createElement("div")
+            divCreate.classList.add("blue");
+            AddElement.append(divCreate);
+    }
+    else if(i == 3) {
+        let AddElement = document.getElementById("all")
+        let divCreate = document.createElement("div")
+        divCreate.classList.add("green");
+        AddElement.append(divCreate);
+    }
 }

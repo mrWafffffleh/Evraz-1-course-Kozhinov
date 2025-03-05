b5705f26c08aabd51bf9462666aa78fb0105edc5
mrWafffffleh <waffleh@mail.ru>
2025-03-05 20:02:01 +0500

    Обновы

--- Товары.js
+++ Товары.js
@@ -1,83 +1,71 @@
+let inputName = document.getElementById("input-name")
+let inputCategory = document.getElementById("selection")
+let inputPrice = document.getElementById("price")
+let inputAmount = document.getElementById("amount")
+let inputDescripton = document.getElementById("desc")
+let right = document.getElementById("right")
+let listDom = document.getElementById("list")
+let inputSale = document.getElementById('salenum')
 
+let categories = {
+    'Products': 'Продукты',
+    'Technic': 'Техника',
+    'Jewellery': 'Украшения',
+    'House': 'Товары для дома',
+    'Clothes': 'Одежда'
+}
+let specialObj = {
+    'B/U': 'Б/У',
+    'fragile': 'Хрупкий'
+}
 function addlist(show){
     let sale = document.getElementById("saleblock")
     if(show){
-        sale.classList.remove("ghost")
-        sale.classList.add("block")
+        sale.style.setProperty('display', 'flex', 'important')
     }
     else{
-        sale.classList.add("ghost")
-        sale.classList.remove("block")
+        sale.style.setProperty('display', 'none', 'important')
     }
 }
 function addcart(){
-    let name = document.getElementById("input-name")
-    let category = document.getElementById("selection")
-    let price = document.getElementById("price")
-    let amount = document.getElementById("amount")
-    let salenum = document.getElementById("salenum")
-    let descripton = document.getElementById("desc")
-    let right = document.getElementById("right")
-    let cart = document.createElement("div")
-    cart.classList.add("cart")
-    let cart_name = document.createElement("div")
-    cart_name.innerText = name.value
-    cart_name.classList.add("cart-name")
-    let cart_cat = document.createElement("div")
-    cart_cat.innerText = category.value
-    cart_cat.classList.add("cart-cat")
-    let cart_desc = document.createElement("div")
-    cart_desc.innerText = descripton.value
-    cart_desc.classList.add("cart-desc")
-    let bottom_cart = document.createElement("div")
-    let pr = document.createElement("span")
-    pr.innerText = "Цена: "
-    let cart_price = document.createElement("span")
-    cart_price.innerText = price.value + " руб. "
-    let am = document.createElement("span")
-    am.innerText = "Количество: "
-    let cart_amount = document.createElement("span")
-    cart_amount.innerText = amount.value
-    let sale = document.createElement("div")
-    let button = document.createElement("button")
-    button.innerText = "X"
-    button.classList.add("button")
-    if(salenum.value != ""){
-        cart_price.classList.add("cross")
-        let a = salenum.value
-        bottom_cart.classList.add("bottom-cart1")
-        sale.innerText = parseInt(price.value) / 100 * a
-        sale.classList.add("sale")
-        right.append(cart)
-        cart.append(button)
-        cart.append(cart_name)
-        cart.append(cart_cat)
-        cart.append(cart_desc)
-        cart.append(bottom_cart)
-        bottom_cart.append(pr)
-        bottom_cart.append(cart_price)
-        bottom_cart.append(am)
-        bottom_cart.append(cart_amount)
-        cart.append(sale)
+    let saleCh = document.querySelector('input[name=sale]:checked')
+
+    let cardT = document.createElement("div")
+    cardT.classList.add("tovar")
+    let price =``
+    if(saleCh == 'yes'){
+        let num = +inputPrice * +inputSale/100
+        let num1 = +inputPrice - num
+        price =` <div class="tovar-price">
+                    <div>Цена: </div>
+                    <div>
+                        <div class="tovar-price-old">Цена: ${inputPrice.value} руб.</div>
+                        <div>${num1} руб.</div>
+                    </div>
+                </div>`
     }
     else{
-        bottom_cart.classList.add("bottom-cart")
-        right.append(cart)
-        cart.append(button)
-        cart.append(cart_name)
-        cart.append(cart_cat)
-        cart.append(cart_desc)
-        cart.append(bottom_cart)
-        bottom_cart.append(pr)
-        bottom_cart.append(cart_price)
-        bottom_cart.append(am)
-        bottom_cart.append(cart_amount)
+        price = `<div class="tovar-price">Цена: ${inputPrice.value}</div>`
     }
-    name.value = ""
-    category.value = ""
-    descripton.value = ""
-    price.value = ""
-    amount.value = ""
-    salenum.value = ""
+    let specials = ``
+    let spec = document.querySelectorAll('input[name=features]:checked')
+    for (let i = 0; i < spec.length; i++) {
+        // console.log(specials[i].value, specialsObj[specials[i].value]);
+        specials += ' ' + specialObj[spec[i].value];
+    }
+    let card = `<div class="tovar-name">${ inputName.value }</div>
+            <div class="tovar-category">${categories[inputCategory.value]}</div>
+            <div class="tovar-specials">${specials}</div>
+            <div class="tovar-description">${inputDescripton.value}</div>
+            <div class="tovar-price-count">
+                ${price}
+                <div class="count">${inputAmount.value}</div>
+            </div>
+            <div class="tovar-close">X</div>`
+    cardT.innerHTML = card
+    listDom.append(cardT)
+
 
 }
+
+
